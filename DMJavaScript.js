9e13ac44f438513fae9900aaf4ff0090ac132817
engineer-only <irfankaleli@yandex.com>
2017-07-06 10:26:06 +0300

    New tutorial file

---
+++ DMJavaScript.js
@@ -0,0 +1,7 @@
+// To run the contents of the directory on a local web server
+// Run the following in the terminal.
+
+> python -m SimpleHTTPServer
+
+// You can reach it from url : localhost:8000
+// with your Web Browser
