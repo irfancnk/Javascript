5ded9d76f86fcb56f689baafbc34fbe19aeda085
IRFAN CAN KALELI <e2035996@cenginek>
2019-03-27 17:16:37 +0300

    Promise All Example Without Rejections

--- tutorial.js
+++ tutorial.js
@@ -3,12 +3,13 @@ var util = require('util');
 
 // JAVASCRIPT TUTORIAL
 
-
+// ===================================== 0 =====================================
 // DIFFERENCE BETWEEN var and let
 // The difference is scoping.
 // var is scoped to the nearest function block.
 // let is scoped to the nearest enclosing block.
 // Both are global if outside any block.
+// Uncomment varLetDifference() call to execute
 function varLetDifference() {
 	if (true) {
 		var varNum = 0;
@@ -23,16 +24,34 @@ function varLetDifference() {
 }
 // varLetDifference();
 
+// ===================================== 0 =====================================
+// =============================================================================
+// =============================================================================
+// =============================================================================
+// =============================================================================
+// =============================================================================
+// ===================================== 1 =====================================
 
 // JAVASCRIPT PROMISE OBJECT
+// Promise.all() method returns a single Promise that resolves when all of the
+// promises passed as an iterable have resolved or when the iterable contains no
+// promises.
 
+var promise1 = Promise.resolve(1);
+var promise2 = 2;
+var promise3 = new Promise(function(resolve, reject) {
+	setTimeout(resolve, 500, 3);
+	// Resolved after 500ms
+});
 
-// var promise1 = Promise.resolve(3);
-// var promise2 = 42;
-// var promise3 = new Promise(function(resolve, reject) {
-//   setTimeout(resolve, 100, 'foo');
-// });
-//
+Promise.all([promise1, promise2, promise3]).then(function(values) {
+	console.log(values);
+});
+
 // Promise.all([promise1, promise2, promise3]).then(function(values) {
 //   console.log(values);
 // });
+
+
+
+// ===================================== 1 =====================================
