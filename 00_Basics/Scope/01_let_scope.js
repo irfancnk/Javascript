e6c773a5de0f307126125d105638b350504528bf
irfancnk <irfankaleli@yandex.com>
2020-12-03 00:18:45 +0300

    Added const scope description.

--- 00_Basics/Scope/01_let_scope.js
+++ 00_Basics/Scope/01_let_scope.js
@@ -1,24 +1,21 @@
 function applyDiscount(prices, percentage) {
-  var discounted = [];
-  for (let i = 0; i < prices.length; i++) {
-    let discountedPrice = prices[i] * (1 - percentage);
-    let finalPrice = Math.round(discountedPrice * 100) / 100;
-    discounted.push(finalPrice);
-  }
+    var discounted = [];
+    for (let i = 0; i < prices.length; i++) {
+        let discountedPrice = prices[i] * (1 - percentage);
+        let finalPrice = Math.round(discountedPrice * 100) / 100;
+        discounted.push(finalPrice);
+    }
+    try {
+        console.log(i); // Reference Error
+        console.log(discountedPrice); // Reference Error
+        console.log(finalPrice); // Reference Error
+    } catch (e) {
 
-  try {
-      console.log(i); // Reference Error
-      console.log(discountedPrice); // Reference Error
-      console.log(finalPrice); // Reference Error
-  } catch (e) {
-
-  }
-
-
-  return discounted;
+    }
+    return discounted;
 }
 
 applyDiscount([100, 150, 200], 0.2);
 
-// Since variables declared with var are function scoped, they are still
-// accessible outside the for loop
+// Since variables declared with let are block scoped, they are not accessible
+// outside the for loop. let is scoped to the nearest enclosing block.
--- 00_Basics/Scope/02_const_scope.js
+++ 00_Basics/Scope/02_const_scope.js
@@ -1,26 +1,17 @@
-//
-//
-// var declaration;
-// console.log(declaration) // undefined
-// // In JavaScript, variables are initialized with the value of undefined when
-// // they are created
-//
-//
-// // Scope defines where variables and functions are accessible inside of your
-// // program. In JavaScript, there are two kinds of scope - global scope, and
-// // function scope. According to the official spec,
-//
-//
-// function getDate () {
-//     var date = new Date();
-//     function formatDate () {
-//         return date.toDateString().slice(4);
-//     }
-//     return formatDate();
-// }
-// getDate();
-// try {
-//     console.log(date);
-// } catch (e) {
-//     // Reference Error
-// }
+function applyDiscount(prices, percentage) {
+    var discounted = [];
+    for (let i = 0; i < prices.length; i++) {
+        const assignOnce = 1;
+    }
+    try {
+        console.log(assignOnce);
+    } catch (e) {
+        console.log(e);
+    }
+    return discounted;
+}
+
+applyDiscount([100], 0.2);
+
+// Since variables declared with const are block scoped, they are not accessible
+// outside the for loop. const is scoped to the nearest enclosing block.
