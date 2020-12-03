0ae867b4ccea0244438d341c55863c1032bd2d39
irfancnk <irfankaleli@yandex.com>
2020-12-04 01:54:12 +0300

    Arrow functions initial file added.

---
+++ 00_Basics/02_Arrow_Functions/00_arrow_function_definition.js
@@ -0,0 +1,15 @@
+// fn declaration
+function add (x,y) {
+    return x + y;
+}
+
+
+// fn expression
+var adder = function (x,y) {
+    return x + y;
+}
+
+
+var adderArror = (x,y) => {
+    return x + y;
+}
